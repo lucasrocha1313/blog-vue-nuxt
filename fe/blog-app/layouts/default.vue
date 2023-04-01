6798fdcc20b0e94a1de4e059c4fbb8e01f8d5d94
Lucas Rocha <a.lucas.a.rocha@gmail.com>
2023-04-01 11:36:17 -0300

    headers

--- fe/blog-app/layouts/default.vue
+++ fe/blog-app/layouts/default.vue
@@ -1,5 +1,7 @@
 <template>
   <div>
+    <TheHeader @sidenavToggle="displaySidenav = !displaySidenav"/>
+    <TheSidenav :show="displaySidenav" @close="displaySidenav = false"/>
     <nuxt/>
   </div>
 </template>
@@ -13,3 +15,19 @@ body {
   margin: 0;
 }
 </style>
+<script>
+import TheHeader from "../components/navigation/the-header.vue";
+import TheSidenav from "../components/navigation/the-sidenav.vue";
+
+export default {
+  components: {
+    TheHeader,
+    TheSidenav
+  },
+  data() {
+    return {
+      displaySidenav: false
+    }
+  }
+}
+</script>
